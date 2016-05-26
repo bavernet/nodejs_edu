f37769b42dbfdc6b417e5f5862d5f431d757e3fd
Sungho Bae <bsh0236@gmail.com>
2016-05-26 17:07:27 +0900

    async module's solution for callback hell

---
+++ day4/async_parallel.js
@@ -0,0 +1,21 @@
+var async = require("async");
+
+async.parallel([ function(callback) {
+	console.log("1st callback");
+	setTimeout(function() {
+		console.log("1st time");
+		callback(null, "one");
+	}, 2000);
+}, function(callback) {
+	console.log("2nd callback");
+	setTimeout(function() {
+		console.log("2nd time");
+		callback(null, "two");
+	}, 100);
+}, function(callback) {
+	console.log("3rd callback");
+	callback(null, "three");
+} ], function(err, results) {
+	console.log('last callback');
+	console.log(arguments);
+});
